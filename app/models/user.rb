a4c659c3b58932ca3dfb17e68683320bbb8e9af1
saksakP0129 <ametoyuki0712@gmail.com>
2020-04-03 21:21:08 +0900

    新規登録画面&ログインの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :phone_number, presence: true,             format: { with: /\A\d{10}$|^\d{11}\z/ }
+  # validates :first_name, format: {/^[ぁ-んァ-ン一-龥]/}
+  # validates :family_name, format: {/^[ぁ-んァ-ン一-龥]/}  
 end
