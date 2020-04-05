b4db99b0875c7a8da7247595735657f413a74481
MTKS-3254-KNT <s.mitsukoshi@gmail.com>
2020-04-05 16:41:54 +0900

    新着商品のimage以外の表示成功

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,24 @@
 class ItemsController < ApplicationController
 
+  # before_action :move_to_index, except: [:index, :show]
+  # ⬆ URL手打ち不正アクセス防止の為
+  # ⬆ ユーザーログイン機能が無い間はコメントアウトにしておきます。
+
   def index
+    @newitems = Item.last(3)
   end
+
 
   def new
   end
 
   def show
+  end
+
+  private
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 
 end
