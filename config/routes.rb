05b76b1d0cfda48860b1244567caf71b5470928e
fujiwara29 <watasixi@gmail.com>
2020-04-06 20:24:50 +0900

    card controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'card/new'
+  get 'card/show'
   devise_for :users
 
   root "items#index"
@@ -7,5 +9,13 @@ Rails.application.routes.draw do
   resources :items 
   resources :orders, only: :index
   resources :users, only: :show
+
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
 
 end
