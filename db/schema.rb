5ce86901f80f5d5aebd8bf482f9b3c3907864bd0
MotoyaSano <motoyasano06@icloud.com>
2020-04-06 15:34:28 +0900

    仮コミット

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_093527) do
+ActiveRecord::Schema.define(version: 0) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -24,45 +24,6 @@ ActiveRecord::Schema.define(version: 2020_04_03_093527) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "image", null: false
-    t.bigint "item_id_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["item_id_id"], name: "index_images_on_item_id_id"
-  end
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.bigint "image_id_id", null: false
-    t.text "introduction", null: false
-    t.bigint "category_id_id", null: false
-    t.bigint "brand_id_id"
-    t.string "condition", null: false
-    t.string "postage_payer", null: false
-    t.string "prefecture_code", null: false
-    t.string "preparation_day", null: false
-    t.integer "price", null: false
-    t.string "trading", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["brand_id_id"], name: "index_items_on_brand_id_id"
-    t.index ["category_id_id"], name: "index_items_on_category_id_id"
-    t.index ["image_id_id"], name: "index_items_on_image_id_id"
-  end
-
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id_id", null: false
     t.bigint "item_id_id", null: false
@@ -72,38 +33,6 @@ ActiveRecord::Schema.define(version: 2020_04_03_093527) do
     t.index ["address_id_id"], name: "index_orders_on_address_id_id"
     t.index ["item_id_id"], name: "index_orders_on_item_id_id"
     t.index ["user_id_id"], name: "index_orders_on_user_id_id"
-  end
-
-  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id_id", null: false
-    t.bigint "item_id_id", null: false
-    t.bigint "address_id_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["address_id_id"], name: "index_orders_on_address_id_id"
-    t.index ["item_id_id"], name: "index_orders_on_item_id_id"
-    t.index ["user_id_id"], name: "index_orders_on_user_id_id"
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "first_name", null: false
-    t.string "family_name", null: false
-    t.string "first_name_kana", null: false
-    t.string "family_name_kana", null: false
-    t.date "birth_year", null: false
-    t.date "birth_month", null: false
-    t.date "birth_day", null: false
-    t.integer "phone_number", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
