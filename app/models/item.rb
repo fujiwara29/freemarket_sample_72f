37257da2d18cdedc3b021a20083285e810c0252b
MTKS-3254-KNT <s.mitsukoshi@gmail.com>
2020-04-13 12:39:23 +0900

    商品出品機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,17 +7,17 @@ class Item < ApplicationRecord
   has_many :orders
   has_many :comments
 
-  validates :name,            presence: true
-  validates :name,            length: { maximum: 40 }
-  validates :introduction,    presence: true
-  validates :introduction,    length: { maximum: 1000 }
-  validates :category_id,     presence: true
-  validates :condition,       presence: true
-  validates :postage_payer,   presence: true
-  validates :prefecture_code, presence: true
-  validates :preparation_day, presence: true
-  validates :price,           numericality: { only_integer: true, greater_than: 300, less_than_or_equal_to: 9999999 }
-  validates :trading,         presence: true
-  validates :user_id,         presence: true
+  # validates :name,            presence: true
+  # validates :name,            length: { maximum: 40 }
+  # validates :introduction,    presence: true
+  # validates :introduction,    length: { maximum: 1000 }
+  # validates :category_id,     presence: true
+  # validates :condition,       presence: true
+  # validates :postage_payer,   presence: true
+  # validates :prefecture_code, presence: true
+  # validates :preparation_day, presence: true
+  # validates :price,           numericality: { only_integer: true, greater_than: 300, less_than_or_equal_to: 9999999 }
+  # validates :trading,         presence: true
+  # validates :user_id,         presence: true
 
 end
