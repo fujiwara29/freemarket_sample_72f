c76aff1fb17d739116297c13008e8aa0584db3d9
kosuke269 <kousuke_playtennis@yahoo.co.jp>
2020-04-15 18:42:00 +0900

    修正済み

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,9 +37,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
+   if item = Item.find(params[:id])
     item.destroy
     redirect_to root_path
+   else
+    redirect_to item_path
+   end
   end
 
   
