bc07072fbde21c894b7a0236af97c7691fd4c8ee
MTKS-3254-KNT <s.mitsukoshi@gmail.com>
2020-04-16 18:35:48 +0900

    商品情報編集ページ体裁作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
-  before_action :set_category, only: [:new, :create]
-  before_action :set_brand, only: [:new, :create]
+  before_action :set_category, only: [:new, :create ,:edit ,:update]
+  before_action :set_brand, only: [:new, :create, :edit, :update]
 
 
   def index
@@ -34,13 +34,35 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    @images = Image.where(item_id: params[:id])
+  end
+
+  def update
+    @item = params[set_item]
+    images = params[:images][:image]
+    respond_to do |format|
+      if @item.valid? && (images.length >= 1) && (images.length <= 10)
+        @item.save
+        images.each do |image|
+          @item.images.create(image: image, item_id: @item.id)
+        end
+        format.html{redirect_to root_path}
+      else
+        @item.images.build
+        format.html{render action: 'new'}
+      end
+    end
+  end
+
   def destroy
     item = Item.find(params[:id])
-   if item.destroy
+    if item.destroy
     redirect_to root_path
-   else
+    else
     redirect_to item_path
-   end
+    end
   end
 
   
