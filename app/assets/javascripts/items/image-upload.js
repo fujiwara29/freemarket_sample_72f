dacf81bfe341452e84de6c4ca633301dc5e860a7
MTKS-3254-KNT <s.mitsukoshi@gmail.com>
2020-04-19 03:18:06 +0900

    出品商品編集機能

--- app/assets/javascripts/items/image-upload.js
+++ app/assets/javascripts/items/image-upload.js
@@ -19,9 +19,11 @@ $(function(){
                     <div class='item-img__content'>
                       <img src=${src}>
                     </div>
-                    <div class='item-img__btn'>
-                      <div class='item-img__btn--delete'>削除</div>
-                    </div>
+                    <label>
+                      <div class='item-img__btn'>
+                        <div class='item-img__btn--delete'>削除</div>
+                      </div>
+                    </label>
                   </div>`
                   
         container.before(html);
@@ -31,8 +33,14 @@ $(function(){
   });
 
 
+
+
   $(document).on("click", '.item-img__btn--delete', function(){
-    var target_image = $(this).parent().parent()
+    var d_id = $(this).attr('id')
+    var d_class = "." + d_id
+    var name = $(d_class).attr('name');
+    $('input:checkbox[name="' + name + '"]').prop('checked', true);
+    var target_image = $(this).parent().parent().parent()
     var target_name = $(target_image).data('image')
     if(file_field.files.length==1){
       $('input[type=file]').val(null)
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,8 @@ class ItemsController < ApplicationController
 
   before_action :set_category, only: [:new, :create ,:edit ,:update]
   before_action :set_brand, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:show ,:edit ,:update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :imag_destroy_params, only: :update
 
   def index
     @newitems = Item.last(3)
@@ -15,12 +16,12 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    images = params[:images][:image]
+    imgs_params = params[:images][:image]
     respond_to do |format|
-      if @item.valid? && (images.length >= 1) && (images.length <= 10)
+      if @item.valid? && (imgs_params.length >= 1) && (imgs_params.length <= 10)
         @item.save
-        images.each do |image|
-          @item.images.update(image: image, item_id: @item.id)
+        imgs_params.each do |image|
+          @item.images.create(image: image, item_id: @item.id)
         end
         format.html{redirect_to root_path}
       else
@@ -34,25 +35,45 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @images = Image.where(item_id: params[:id])
   end
 
   def update
-    @item = params[set_item]
-    images = params[:images][:image]
     respond_to do |format|
-    if @item.valid? && (images.length >= 1) && (images.length <= 10)
-      @item.save
-      @old_images = Image.whare(item_id:params[:id])
-      @old_images.destroy
-      images.each do |image|
-        @item.images.update(image: image, item_id: @item.id)
+      # 編集した商品情報の更新
+      @item.update(item_params)
+      
+      # 画像を追加した場合の処理
+      if params[:images].nil?
+      else
+        imgs_params = params[:images][:image]
+        if (imgs_params.length >= 1) && (imgs_params.length <= 10)
+          imgs_params.each do |image|
+            Image.create(image: image, item_id: @item.id)
+          end
+        end
       end
-      format.html{redirect_to root_path}
-    else
-      flash.now[:alert] = '必須項目が入力されていません。'
-      @item.images.build
-      format.html{render action: 'edit'}
+      
+      #DBの画像が0になる場合は処理をしないようにする
+      db_image = Image.where(item_id: @item.id)
+      d_box = []
+      @item.images.each do |d_image|
+        if params[:item][:imag_destroy][:"#{d_image.id}"] == "1"
+        d_box << d_image
+        end
+      end
+      
+      # データベースのimage数と削除image数を比較
+      if db_image.length > d_box.length
+        # 既存の画像を削除した場合の処理
+        d_box.each do |d_image2|
+          destroimage = Image.find_by(id: d_image2.id)
+          destroimage.destroy
+        end
+      else
+        # image数が0になる場合の処理
+        format.html{redirect_to edit_item_path, notice: '画像は1枚以上登録してください！'}
+      end
+    format.html{redirect_to item_path}
     end
   end
 
@@ -64,90 +85,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  
-  private
-
-  def set_category
-    @category = Category.all()
-  end
-
-  def set_brand
-    @brand = []
-    # @brand = Bland.all()
-  end
-
-  def item_params
-    params.require(:item)
-    .permit(
-      :name,
-      :introduction,
-      :category_id,
-      :brand_id,
-      :condition,
-      :postage_payer,
-      :prefecture_code,
-      :preparation_day,
-      :price,
-    ).merge(
-      trading: "販売中",
-      user_id: current_user.id
-    )
-  end
-
-end
-
-
-# class ItemsController < ApplicationController
-
-
-#   def index
-#     @newitems = Item.last(3)
-#   end
-
-#   def new
-#     @item = Item.new
-#     @item.images.build
-#   end
-
-  # def create
-  #   @item = Item.new(item_params)
-  #   images = params[:images][:image]
-  #   respond_to do |format|
-  #     if @item.valid? && (images.length >= 1) && (images.length <= 10)
-  #       @item.save
-  #       images.each do |image|
-  #         @item.images.create(image: image, item_id: @item.id)
-  #       end
-  #       format.html{redirect_to root_path}
-  #     else
-  #       @item.images.build
-  #       format.html{render action: 'new'}
-  #     end
-  #   end
-  # end
-
-  # def show
-  # end
-
-  # def destroy
-  #   if @item.destroy
-  #   redirect_to root_path
-  #   else
-  #   redirect_to item_path
-  #   end
-  # end
-
-  # def edit
-  # end
-
-  # def update
-  #   if @item.update(item_params)
-  #   else
-  #     flash.now[:alert] = '必須項目が入力されていません。'
-  #     redirect_to edit
-  #   end
-  # end
-  
   private
   
   def set_item
@@ -179,6 +116,10 @@ end
       trading: "販売中",
       user_id: current_user.id
     )
+  end
+
+  def imag_destroy_params
+    params.require(:item).permit(:imag_destroy )
   end
 
 end
