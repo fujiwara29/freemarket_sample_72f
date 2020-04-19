7168a36fcefc183ef827362e1b050168be39b644
MTKS-3254-KNT <s.mitsukoshi@gmail.com>
2020-04-19 23:25:17 +0900

    商品一覧表示改造

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @newitems = Item.last(3)
+    @newitems = Item.last(15)
   end
 
   def new
