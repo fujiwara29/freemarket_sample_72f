e836a842a358aecd4a54f2505e82fb339f40a7ea
MTKS-3254-KNT <s.mitsukoshi@gmail.com>
2020-04-21 06:52:10 +0900

    カテゴリ検索、Blancの場合の条件式追加

--- app/controllers/items/searches_controller.rb
+++ app/controllers/items/searches_controller.rb
@@ -1,19 +1,11 @@
 class Items::SearchesController < ApplicationController
 
-  before_action :category_search
-
   def index
     if params[:keyword].nil?
-      @category_items = Item.category_like(category_search).order("created_at DESC")
+      @category_items = Item.category_like(params[:format]).order("created_at DESC")
     else
       @items = Item.name_like(params[:keyword]).order("created_at DESC")
     end
-  end
-
-private
-
-  def category_search
-    params[:format]
   end
 
 end
